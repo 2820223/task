eb6c311503875de31e124b7937fad1f56ced478c
PC671 <missbahodirova@gmail.com>
2022-12-09 10:56:04 +0500

    This is comment

---
+++ app/src/main/java/uz/itschool/myapplicationeee/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.itschool.myapplicationeee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
